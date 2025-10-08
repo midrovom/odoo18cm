38007ff412fa805f8565712fc2584339caffc297
midrovom <midrovo@callphoneecuador.com>
2025-10-08 10:18:30 -0500

    fix-update: actualizando

---
+++ website_conedera/__manifest__.py
@@ -0,0 +1,29 @@
+{
+    'name': 'Theme Conedera',
+    'version': '18.0.1.0.0',
+    'description': '''
+        Modulo personalizado para Odoo 18 CM
+        Personalizacion de temas y snippets para la página de Conedera
+    ''',
+    'summary': 'Temas y snippets personalizados para Conedera',
+    'author': 'Ing. Mauricio Idrovo',
+    'website': 'https://callphoneecuador.com',
+    'license': 'LGPL-3',
+    'category': 'Website',
+    'images': [
+        'static/description/conedera_screenshot.gif',
+    ],
+    'depends': [
+        'website',
+        'theme_prime',
+    ],
+    'data': [
+        'views/headers.xml',
+        'views/snippets.xml'
+    ],
+    'auto_install': False,
+    'application': False,
+    'assets': {
+        
+    },
+}
