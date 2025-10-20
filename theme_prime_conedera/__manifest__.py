a53a18e2282439f436f61d6c14cb0110946e88c8
midrovom <midrovo@callphoneecuador.com>
2025-10-20 16:58:24 -0500

    fix-update: actualizando theme conedera

--- theme_prime_conedera/__manifest__.py
+++ theme_prime_conedera/__manifest__.py
@@ -20,6 +20,8 @@
     'data': [
         'views/headers.xml',
         'views/snippets/s_cover_1.xml',
+        'views/snippets/dynamic_snippets.xml',
+        'views/snippets.xml',
         # 'views/website_template.xml'
     ],
     'auto_install': False,
