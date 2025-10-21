fea60e039e0e17b1da28b48a6f1d84b78ed935b8
midrovom <midrovo@callphoneecuador.com>
2025-10-21 11:10:00 -0500

    fix-update: actualizando theme conedera

--- theme_prime_conedera/static/src/components/registries.js
+++ theme_prime_conedera/static/src/components/registries.js
@@ -4,7 +4,7 @@ import { registry } from '@web/core/registry';
 // All of our work will be trash because registry will be no longer useful in next version.
 let PRODUCTS_ACTIONS = ['rating', 'quick_view', 'add_to_cart', 'comparison', 'wishlist', 'category_info', 'label'];
 let PRODUCTS_ACTIONS_2 = ['rating', 'category_info', 'add_to_cart', 'wishlist', 'comparison', 'description_ecommerce', 'label'];
-let PRODUCTS_DATA = { models: ['product.template', 'product.product', 'product.atribute'], fields: ['name', 'list_price', 'dr_stock_label'], fieldsToMarkUp: ['price', 'list_price', 'dr_stock_label']}
+let PRODUCTS_DATA = { models: ['product.template', 'product.product'], fields: ['name', 'list_price', 'dr_stock_label'], fieldsToMarkUp: ['price', 'list_price', 'dr_stock_label']}
 let CATEGORIES_DATA = { fields: ['name'], fieldsToMarkUp: []};
 let SELECTOR_DATA = { TpRecordSelector: { ...PRODUCTS_DATA, defaultVal: { selectionType: 'manual', recordsIDs: [], model: 'product.template'}}};
 let EXTRA_OPTIONS = { TpExtraOpts: { startDate: '', endDate: '', priceList: '*' } };
