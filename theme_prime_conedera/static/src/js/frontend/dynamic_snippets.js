10cc7df4acc88551d29d84dfc9d88a0de2fc8d44
midrovom <midrovo@callphoneecuador.com>
2025-10-22 16:19:10 -0500

    fix-update: actualizando theme conedera

--- theme_prime_conedera/static/src/js/frontend/dynamic_snippets.js
+++ theme_prime_conedera/static/src/js/frontend/dynamic_snippets.js
@@ -42,54 +42,54 @@ publicWidget.registry.s_d_image_products_block_conedera = ProductRootWidget.exte
 });
 
 
-publicWidget.registry.product_brand_tabs = ProductRootWidget.extend(ProductsBlockMixins, {
-    selector: '.s_d_product_count_down',
+// publicWidget.registry.product_brand_tabs = ProductRootWidget.extend(ProductsBlockMixins, {
+//     selector: '.s_d_product_count_down',
 
-    bodyTemplate: 's_d_product_count_down_template',
-    bodySelector: '.product_brand_tabs',
+//     bodyTemplate: 's_d_product_count_down_template',
+//     bodySelector: '.product_brand_tabs',
 
-    snippetNodeAttrs: (ProductRootWidget.prototype.snippetNodeAttrs || []).concat(['data-selection-info']),
+//     snippetNodeAttrs: (ProductRootWidget.prototype.snippetNodeAttrs || []).concat(['data-selection-info']),
 
-    controllerRoute: '/theme_prime/get_products_data',
+//     controllerRoute: '/theme_prime/get_products_data',
 
-    fieldstoFetch: ['name', 'description_ecommerce', 'rating', 'public_categ_ids', 'offer_data'],
+//     fieldstoFetch: ['name', 'description_ecommerce', 'rating', 'public_categ_ids', 'offer_data'],
 
-    extraLibs: (ProductRootWidget.prototype.extraLibs || []).concat(['/theme_prime/static/lib/OwlCarousel2-2.3.4/owl.carousel.js']),
-    /**
-     * @private
-     */
-    _getOptions: function () {
-        var options = this._super.apply(this, arguments);
-        if (this.selectionType) {
-            options = options || {};
-            options['shop_config_params'] = true;
-        }
-        return options;
-    },
-    /**
-     * @private
-     */
-    _setDBData: function (data) {
-        this.shopParams = data.shop_config_params;
-        this._super.apply(this, arguments);
-    },
-    /**
-     * initialize owlCarousel.
-     * @private
-     */
-    _modifyElementsAfterAppend: function () {
-        this._super.apply(this, arguments);
-        this._reloadWidget({ selector: '.tp-countdown' });
-        this.$('.droggol_product_slider_top').owlCarousel({
-            dots: false,
-            margin: 20,
-            stagePadding: 5,
-            rewind: true,
-            rtl: localization.direction === 'rtl',
-            nav: true,
-            navText: ['<i class="dri h4 dri-chevron-left-l"></i>', '<i class="dri h4 dri-chevron-right-l"></i>'],
-            responsive: {0: {items: 1}, 768: {items: 2}, 992: {items: 1}, 1200: {items: 1},
-            },
-        });
-    },
-});
+//     extraLibs: (ProductRootWidget.prototype.extraLibs || []).concat(['/theme_prime/static/lib/OwlCarousel2-2.3.4/owl.carousel.js']),
+//     /**
+//      * @private
+//      */
+//     _getOptions: function () {
+//         var options = this._super.apply(this, arguments);
+//         if (this.selectionType) {
+//             options = options || {};
+//             options['shop_config_params'] = true;
+//         }
+//         return options;
+//     },
+//     /**
+//      * @private
+//      */
+//     _setDBData: function (data) {
+//         this.shopParams = data.shop_config_params;
+//         this._super.apply(this, arguments);
+//     },
+//     /**
+//      * initialize owlCarousel.
+//      * @private
+//      */
+//     _modifyElementsAfterAppend: function () {
+//         this._super.apply(this, arguments);
+//         this._reloadWidget({ selector: '.tp-countdown' });
+//         this.$('.droggol_product_slider_top').owlCarousel({
+//             dots: false,
+//             margin: 20,
+//             stagePadding: 5,
+//             rewind: true,
+//             rtl: localization.direction === 'rtl',
+//             nav: true,
+//             navText: ['<i class="dri h4 dri-chevron-left-l"></i>', '<i class="dri h4 dri-chevron-right-l"></i>'],
+//             responsive: {0: {items: 1}, 768: {items: 2}, 992: {items: 1}, 1200: {items: 1},
+//             },
+//         });
+//     },
+// });
