6741b5ee3f779d4f5ed5e525b8a1145487395b3d
midrovom <midrovo@callphoneecuador.com>
2025-10-22 17:21:39 -0500

    fix-update: actualizando theme conedera

--- theme_prime_conedera/static/src/components/registries.js
+++ theme_prime_conedera/static/src/components/registries.js
@@ -13,7 +13,7 @@ let EXTRA_OPTIONS = { TpExtraOpts: { startDate: '', endDate: '', priceList: '*' 
 // Registrar snippet en el registry
 registry.category('theme_prime_snippet_registry')
     .add('s_d_image_products_block_conedera', { 
-        template: 'theme_prime_conedera.s_d_image_products_block_conedera',  // <--- Aquí tu template
+        template: 's_d_image_products_block_conedera',  // <--- Aquí tu template
         widgets: { ...SELECTOR_DATA, ...EXTRA_OPTIONS }, 
         defaultValue: { hasSwitcher: true }
     });
