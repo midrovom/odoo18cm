dcd9cf2c3165fa4c54e926664bee5c8ecdc3f2cd
midrovom <midrovo@callphoneecuador.com>
2025-10-22 14:08:40 -0500

    fix-update: actualizando theme conedera

--- theme_prime_conedera/__manifest__.py
+++ theme_prime_conedera/__manifest__.py
@@ -26,13 +26,13 @@
     'auto_install': False,
     'application': False,
     'assets': {
-        'web.assets_frontend': [
-            # 'theme_prime_conedera/static/src/js/frontend/dynamic_snippets.js',
-            'theme_prime_conedera/static/src/xml/frontend/s_image_products.xml',
-        ],
-        # 'website.assets_wysiwyg': [
-        #     'theme_prime_conedera/static/src/components/registries.js'
+        # 'web.assets_frontend': [
+        #     # 'theme_prime_conedera/static/src/js/frontend/dynamic_snippets.js',
+        #     'theme_prime_conedera/static/src/xml/frontend/s_image_products.xml',
         # ],
+        # # 'website.assets_wysiwyg': [
+        # #     'theme_prime_conedera/static/src/components/registries.js'
+        # # ],
         
     },
 }
