deb3a674889dce5332bee9d2c233549f70078e47
midrovom <midrovo@callphoneecuador.com>
2025-12-05 16:14:50 -0500

    fix-update:...

--- field_wassenger/models/product.py
+++ field_wassenger/models/product.py
@@ -3,13 +3,13 @@ from odoo import _, api, fields, models
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    product_pauta_ids = fields.One2many('product.pauta', 'product_tmpl_id', string='Pautas')
+    product_pauta_ids = fields.Many2many('product.pauta', string='Pautas')
 
 
 class ProductPauta(models.Model):
     _name = 'product.pauta'
     _description = 'Pauta del producto'
 
-    product_tmpl_id = fields.Many2one('product.template', string='Producto')
+    name = fields.Char('Nombre')
     
     
